bf5b587438a25fd2c760ae58e68427203dea0f0b
LorD ClockaN <davor@losinj.com>
2011-07-14 22:35:37 +0200

    ADD: new WORKQUE code from 36.1 and rest of kernel patched dangerously by Imoseyon - modified governors by LorD ClockaN
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_brazilianwax.c
    	drivers/cpufreq/cpufreq_interactive.c
    	drivers/cpufreq/cpufreq_smartass.c
    	drivers/video/msm/msm_fb.c

--- init/main.c
+++ init/main.c
@@ -804,7 +804,7 @@ static void __init do_initcalls(void)
  */
 static void __init do_basic_setup(void)
 {
-	init_workqueues();
+//	init_workqueues();
 	cpuset_init_smp();
 	usermodehelper_init();
 	init_tmpfs();
