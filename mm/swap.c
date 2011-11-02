c6fa9dc0a8650ef53ab3fc673bedde47537bcb0b
LorD ClockaN <davor@losinj.com>
2011-11-02 17:54:19 +0100

    mm: check PageUnevictable in lru_deactivate_fn()
        The lru_deactivate_fn should not move page which in on unevictable lru
        into inactive list.  Otherwise, we can meet BUG when we use
        isolate_lru_pages as __isolate_lru_page could return -EINVAL.
    
        Reported-by: Ying Han <yinghan@google.com>
        Tested-by: Ying Han <yinghan@google.com>
        Signed-off-by: Minchan Kim <minchan.kim@gmail.com>
        Reviewed-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
        Reviewed-by: Rik van Riel<riel@redhat.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/swap.c
+++ mm/swap.c
@@ -317,6 +317,9 @@ static void lru_deactivate_fn(struct page *page, void *arg)
 	if (!PageLRU(page))
 		return;
 
+	if (PageUnevictable(page))
+		return;
+
 	/* Some processes are using the page */
 	if (page_mapped(page))
 		return;
