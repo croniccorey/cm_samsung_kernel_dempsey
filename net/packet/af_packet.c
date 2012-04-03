d24418e0327d97f657c5671e6b0b9742f0105e8b
Pasquale Lodise <pakilodi@gmail.com>
2012-04-03 12:14:53 +0200

    net: Data usage now working (thanks @adfad666, @lupohirp, @anroidmeda)
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Signed-off-by: scotthartbti <scott@ubuntu.com>
    
    Conflicts:
    	.config
    	arch/arm/configs/cyanogenmod_thunderg_defconfig
    	include/linux/cpufreq.h
    	net/appletalk/Makefile
    	net/appletalk/aarp.c
    	net/appletalk/atalk_proc.c
    	net/appletalk/ddp.c
    	net/appletalk/dev.c
    	net/appletalk/sysctl_net_atalk.c
    	net/ieee802154/Kconfig
    	net/ieee802154/Makefile
    	net/ieee802154/af802154.h
    	net/ieee802154/af_ieee802154.c
    	net/ieee802154/dgram.c
    	net/ieee802154/ieee802154.h
    	net/ieee802154/netlink.c
    	net/ieee802154/nl-mac.c
    	net/ieee802154/nl-phy.c
    	net/ieee802154/nl_policy.c
    	net/ieee802154/raw.c
    	net/ieee802154/wpan-class.c
    	net/netfilter/xt_TPROXY.c
    	net/netfilter/xt_rateest.c
    	net/netfilter/xt_socket.c
    	net/netfilter/xt_tcpmss.c
    	net/wireless/nl80211.c
    
    Conflicts:
    	include/linux/jhash.h
    	include/net/netfilter/nf_conntrack.h
    	net/ipv6/inet6_connection_sock.c
    	net/ipv6/reassembly.c

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -746,7 +746,6 @@ static int tpacket_rcv(struct sk_buff *skb, struct net_device *dev,
 		h.h2->tp_sec = ts.tv_sec;
 		h.h2->tp_nsec = ts.tv_nsec;
 		h.h2->tp_vlan_tci = vlan_tx_tag_get(skb);
-		h.h2->tp_padding = 0;
 		hdrlen = sizeof(*h.h2);
 		break;
 	default:
@@ -1585,10 +1584,8 @@ static int packet_recvmsg(struct kiocb *iocb, struct socket *sock,
 
 		err = -EINVAL;
 		vnet_hdr_len = sizeof(vnet_hdr);
-		if (len < vnet_hdr_len)
+		if ((len -= vnet_hdr_len) < 0)
 			goto out_free;
-
-		len -= vnet_hdr_len;
 
 		if (skb_is_gso(skb)) {
 			struct skb_shared_info *sinfo = skb_shinfo(skb);
@@ -1668,7 +1665,6 @@ static int packet_recvmsg(struct kiocb *iocb, struct socket *sock,
 		aux.tp_net = skb_network_offset(skb);
 		aux.tp_vlan_tci = vlan_tx_tag_get(skb);
 
-		aux.tp_padding = 0;
 		put_cmsg(msg, SOL_PACKET, PACKET_AUXDATA, sizeof(aux), &aux);
 	}
 
@@ -1697,7 +1693,7 @@ static int packet_getname_spkt(struct socket *sock, struct sockaddr *uaddr,
 	rcu_read_lock();
 	dev = dev_get_by_index_rcu(sock_net(sk), pkt_sk(sk)->ifindex);
 	if (dev)
-		strncpy(uaddr->sa_data, dev->name, 14);
+		strlcpy(uaddr->sa_data, dev->name, 15);
 	else
 		memset(uaddr->sa_data, 0, 14);
 	rcu_read_unlock();
@@ -1720,7 +1716,6 @@ static int packet_getname(struct socket *sock, struct sockaddr *uaddr,
 	sll->sll_family = AF_PACKET;
 	sll->sll_ifindex = po->ifindex;
 	sll->sll_protocol = po->num;
-	sll->sll_pkttype = 0;
 	rcu_read_lock();
 	dev = dev_get_by_index_rcu(sock_net(sk), po->ifindex);
 	if (dev) {
