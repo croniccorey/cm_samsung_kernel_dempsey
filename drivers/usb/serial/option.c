e080d367f8ca007cfd04498756c073ba2a47af0a
scotthartbti <scott.hart.bti@gmail.com>
2012-09-09 14:00:37 -0700

    USB: option: fix memory leak Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -47,6 +47,7 @@
 /* Function prototypes */
 static int  option_probe(struct usb_serial *serial,
 			const struct usb_device_id *id);
+static void option_release(struct usb_serial *serial);
 static int option_send_setup(struct usb_serial_port *port);
 static void option_instat_callback(struct urb *urb);
 
@@ -940,7 +941,7 @@ static struct usb_serial_driver option_1port_device = {
 	.tiocmset          = usb_wwan_tiocmset,
 	.attach            = usb_wwan_startup,
 	.disconnect        = usb_wwan_disconnect,
-	.release           = usb_wwan_release,
+	.release           = option_release,
 	.read_int_callback = option_instat_callback,
 #ifdef CONFIG_PM
 	.suspend           = usb_wwan_suspend,
@@ -1058,6 +1059,15 @@ static enum option_blacklist_reason is_blacklisted(const u8 ifnum,
 		}
 	}
 	return OPTION_BLACKLIST_NONE;
+}
+
+static void option_release(struct usb_serial *serial)
+{
+struct usb_wwan_intf_private *priv = usb_get_serial_data(serial);
+
+usb_wwan_release(serial);
+
+kfree(priv);
 }
 
 static void option_instat_callback(struct urb *urb)
