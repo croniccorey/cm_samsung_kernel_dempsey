cb3eb9b406ddd5f542477ee5ff3f266293d40b58
scotthartbti <scott.hart.bti@gmail.com>
2012-10-05 11:22:46 -0700

    samsung_infuse4g: Add 50mb to /system from /cache Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- drivers/mtd/onenand/samsung_infuse4g.h
+++ drivers/mtd/onenand/samsung_infuse4g.h
@@ -42,12 +42,12 @@ struct mtd_partition s3c_partition_info[] = {
 	{	
 		.name		= "system",
 		.offset		=  (132*SZ_256K),
-		.size		= (1400*SZ_256K), //1531
+		.size		= (1600*SZ_256K), //1531
 	},
 	{
 		.name		= "cache",
-		.offset		= (1532*SZ_256K),
-		.size		= (342*SZ_256K), //1873
+		.offset		= (1732*SZ_256K),
+		.size		= (142*SZ_256K), //1873
 	},
 	{       /* we should consider moving this before the modem at the end
 	           that would allow us to change the partitions before without
