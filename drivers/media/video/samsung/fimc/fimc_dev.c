f87010fde7a1627c959a1ba6207187cd45073f19
scotthartbti <scott.hart.bti@gmail.com>
2013-04-07 22:34:37 -0400

    Fix fimc device resume. Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- drivers/media/video/samsung/fimc/fimc_dev.c
+++ drivers/media/video/samsung/fimc/fimc_dev.c
@@ -1719,12 +1719,14 @@ int fimc_resume(struct platform_device *pdev)
 {
 	struct fimc_control *ctrl;
 	struct s3c_platform_fimc *pdata;
-	int id = pdev->id;
+	int in_use, id = pdev->id; 
 
 	ctrl = get_fimc_ctrl(id);
 	pdata = to_fimc_plat(ctrl->dev);
 
-	if (atomic_read(&ctrl->in_use) && ctrl->status != FIMC_OFF_SLEEP)
+	in_use = atomic_read(&ctrl->in_use);
+
+  	if (in_use) 
 		fimc_clk_en(ctrl, true);
 
 	if (ctrl->out)
@@ -1733,6 +1735,9 @@ int fimc_resume(struct platform_device *pdev)
 		fimc_resume_cap(ctrl);
 	else
 		ctrl->status = FIMC_STREAMOFF;
+
+	if (in_use && 0 != ctrl->id)
+    	fimc_clk_en(ctrl, false); 
 
 	return 0;
 }
