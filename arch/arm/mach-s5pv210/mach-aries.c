0e2565625c2322107575d6cace2e77d31bee9b05
scotthartbti <scott.hart.bti@gmail.com>
2013-04-23 20:07:44 -0400

    Improve usb signal Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -7947,8 +7947,8 @@ void otg_phy_init(void)
 /*	writel((readl(S3C_USBOTG_PHYTUNE) & ~(0xf)) | (0x1<<2) | (0x1<<1),
 			S3C_USBOTG_PHYTUNE);         eye-diagram tuning      */
 	
-	writel((readl(S3C_USBOTG_PHYTUNE) & ~(0xf)) | (0xf),
-			S3C_USBOTG_PHYTUNE);
+	/* set DC level as 0xf (24%) */
+  writel(readl(S3C_USBOTG_PHYTUNE) | 0xf, S3C_USBOTG_PHYTUNE);
 	
 }
 EXPORT_SYMBOL(otg_phy_init);
