893f91b5ef9c556e4075897958aad17106c8444c
scotthartbti <scott.hart.bti@gmail.com>
2013-04-30 01:58:09 -0400

    Misc fixes Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
@@ -605,14 +605,21 @@ static void dhd_set_packet_filter(int value, dhd_pub_t *dhd)
 }
 
 #if defined(CONFIG_HAS_EARLYSUSPEND)
+static int wifi_pm = 0;
+module_param(wifi_pm, int, 0755);
+
 static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 {
 #ifndef CUSTOMER_HW_SAMSUNG
-	int power_mode = PM_MAX;
+int power_mode = PM_MAX;
 	/* wl_pkt_filter_enable_t	enable_parm; */
+
 	char iovbuf[32];
 	int bcn_li_dtim = 3;
 	uint roamvar = 1;
+
+if ( wifi_pm == 1 )
+    power_mode = PM_FAST;
 #endif
 
 	DHD_ERROR(("%s: enter, value = %d in_suspend=%d\n",
