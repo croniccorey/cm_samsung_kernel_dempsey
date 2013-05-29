1786035f60e328e568eb6b7565f2033a7013b1ac
scotthartbti <scott.hart.bti@gmail.com>
2013-05-29 17:46:48 -0400

    fix xt_qtaguid again Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -26,10 +26,6 @@
 #include <net/tcp.h>
 #include <net/udp.h>
 
-#if defined(CONFIG_IP6_NF_IPTABLES) || defined(CONFIG_IP6_NF_IPTABLES_MODULE)
-#include <linux/netfilter_ipv6/ip6_tables.h>
-#endif
-
 #include <linux/netfilter/xt_socket.h>
 #include "xt_qtaguid_internal.h"
 #include "xt_qtaguid_print.h"
@@ -820,18 +816,18 @@ static int iface_stat_all_proc_read(char *page, char **num_items_returned,
 		} else {
 			stats = &no_dev_stats;
 		}
-    		len = snprintf(outp, char_count,
-             "%s %d "
-             "%llu %llu %llu %llu "
-             "%llu %llu %llu %llu\n",
-             iface_entry->ifname,
-             iface_entry->active,
-             iface_entry->totals[IFS_RX].bytes,
-             iface_entry->totals[IFS_RX].packets,
-             iface_entry->totals[IFS_TX].bytes,
-             iface_entry->totals[IFS_TX].packets,
-             stats->rx_bytes, stats->rx_packets,
-             stats->tx_bytes, stats->tx_packets);
+		len = snprintf(outp, char_count,
+			       "%s %d "
+			       "%llu %llu %llu %llu "
+			       "%lu %lu %lu %lu\n",
+			       iface_entry->ifname,
+			       iface_entry->active,
+			       iface_entry->totals[IFS_RX].bytes,
+			       iface_entry->totals[IFS_RX].packets,
+			       iface_entry->totals[IFS_TX].bytes,
+			       iface_entry->totals[IFS_TX].packets,
+			       stats->rx_bytes, stats->rx_packets,
+			       stats->tx_bytes, stats->tx_packets);
 		if (len >= char_count) {
 			spin_unlock_bh(&iface_stat_list_lock);
 			*outp = '\0';
@@ -1269,7 +1265,7 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 	struct data_counters *uid_tag_counters;
 	struct sock_tag *sock_tag_entry;
 	struct iface_stat *iface_entry;
-	struct tag_stat *new_tag_stat = NULL;
+	struct tag_stat *new_tag_stat;
 	MT_DEBUG("qtaguid: if_tag_stat_update(ifname=%s "
 		"uid=%u sk=%p dir=%d proto=%d bytes=%d)\n",
 		 ifname, uid, sk, direction, proto, bytes);
@@ -1334,19 +1330,8 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 	}
 
 	if (acct_tag) {
-		/* Create the child {acct_tag, uid_tag} and hook up parent. */
 		new_tag_stat = create_if_tag_stat(iface_entry, tag);
 		new_tag_stat->parent_counters = uid_tag_counters;
-	} else {
-    	/*
-       	 * For new_tag_stat to be still NULL here would require:
-    	 *  {0, uid_tag} exists
-    	 *  and {acct_tag, uid_tag} doesn't exist
-    	 *  AND acct_tag == 0.
-    	 * Impossible. This reassures us that new_tag_stat
-    	 * below will always be assigned.
-    	 */
-    	BUG_ON(!new_tag_stat);
 	}
 	tag_stat_update(new_tag_stat, direction, proto, bytes);
 	spin_unlock_bh(&iface_entry->tag_stat_list_lock);
@@ -1473,6 +1458,7 @@ static int __init iface_stat_init(struct proc_dir_entry *parent_procdir)
 	}
 	iface_stat_all_procfile->read_proc = iface_stat_all_proc_read;
 
+
 	err = register_netdevice_notifier(&iface_netdev_notifier_blk);
 	if (err) {
 		pr_err("qtaguid: iface_stat: init "
@@ -1551,27 +1537,6 @@ static struct sock *qtaguid_find_sk(const struct sk_buff *skb,
 	return sk;
 }
 
-static int ipx_proto(const struct sk_buff *skb,
-         struct xt_action_param *par)
-{
-  int thoff, tproto;
-
-  switch (par->family) {
-  case NFPROTO_IPV6:
-    tproto = ipv6_find_hdr(skb, &thoff, -1, NULL);
-    if (tproto < 0)
-      MT_DEBUG("%s(): transport header not found in ipv6"
-         " skb=%p\n", __func__, skb);
-    break;
-  case NFPROTO_IPV4:
-    tproto = ip_hdr(skb)->protocol;
-    break;
-  default:
-    tproto = IPPROTO_RAW;
-  }
-  return tproto;
-}
-
 static void account_for_uid(const struct sk_buff *skb,
 			    const struct sock *alternate_sk, uid_t uid,
 			    struct xt_action_param *par)
@@ -1598,15 +1563,15 @@ static void account_for_uid(const struct sk_buff *skb,
 	} else if (unlikely(!el_dev->name)) {
 		pr_info("qtaguid[%d]: no dev->name?!!\n", par->hooknum);
 	} else {
-		int proto = ipx_proto(skb, par);
-    		MT_DEBUG("qtaguid[%d]: dev name=%s type=%d fam=%d proto=%d\n",
-	       		par->hooknum, el_dev->name, el_dev->type,
-       			par->family, proto);
+		MT_DEBUG("qtaguid[%d]: dev name=%s type=%d\n",
+			 par->hooknum,
+			 el_dev->name,
+			 el_dev->type);
 
 		if_tag_stat_update(el_dev->name, uid,
 				skb->sk ? skb->sk : alternate_sk,
 				par->in ? IFS_RX : IFS_TX,
-				proto, skb->len);
+				ip_hdr(skb)->protocol, skb->len);
 	}
 }
 
@@ -1651,8 +1616,8 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	} else {
 		atomic64_inc(&qtu_events.match_found_sk);
 	}
-	MT_DEBUG("qtaguid[%d]: sk=%p got_sock=%d fam=%d proto=%d\n",
-		par->hooknum, sk, got_sock, par->family, ipx_proto(skb, par));
+	MT_DEBUG("qtaguid[%d]: sk=%p got_sock=%d proto=%d\n",
+		par->hooknum, sk, got_sock, ip_hdr(skb)->protocol);
 	if (sk != NULL) {
 		MT_DEBUG("qtaguid[%d]: sk=%p->sk_socket=%p->file=%p\n",
 			par->hooknum, sk, sk->sk_socket,
@@ -2533,7 +2498,7 @@ static int qtaguid_stats_proc_read(char *page, char **num_items_returned,
 		return len;
 	}
 
-       CT_DEBUG("qtaguid:proc stats page=%p *num_items_returned=%p off=%ld "
+	CT_DEBUG("qtaguid:proc stats page=%p *num_items_returned=%p off=%ld "
 		"char_count=%d *eof=%d\n", page, *num_items_returned,
 		items_to_skip, char_count, *eof);
 
